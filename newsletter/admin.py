fba53ba7c9289bb2234df8915cb24c793cd82284
yampi <cdryampi@gmail.com>
2024-01-08 13:50:29 +0100

    modal newsletter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -24,7 +24,8 @@ class NewsletterAdmin(admin.ModelAdmin):
                 "<p>Assegura't d'adaptar la plantilla si cal.</p>"
                 "<p>Els enllaços del tracking estan limitats pels logos de la web. Una vegada generada la plantilla el pots modificar manualment els enllaços. Però cal que l'enllaç arribi cap a la web.</p>"
                 "<p>El subtítol és per poder afegir un nom de l'esdeveniment especial, ha de tenir relació amb la temàtica.</p>"
-                "<p><strong>Nota:</strong> Pots esborrar totes les plantilles des del llistat de newsletter, així no omplim el servidor de fitxers innecessaris des de Personalització.</p>"
+                "<p><strong>Nota:</strong> Pots esborrar totes les plantilles des de Personalització, així no omplim el servidor de fitxers innecessaris.</p>"
+                "<p>Pots tornar a generar la plantilla tantes vegades com vulguis des del llistat de newsletter.</p>"
             ),
         }),
     ]
